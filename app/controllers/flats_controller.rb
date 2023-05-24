2e0cc110fdb5029edf9c9ebb310aa280af06f22a
Etienne Leguay <etienneleguay@icloud.com>
2023-05-24 16:57:44 -0400

    Air bnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,26 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+100.times {
+  @flat = Flat.new
+  @flat.address = Faker::Address.street_address
+  @flat.name = Faker::Movies::HarryPotter.location
+  @flat.description = Faker::Movies::HarryPotter.quote
+  @flat.price_per_night = (50..700).to_a.sample
+  @flat.number_of_guests = (1..5).to_a.sample
+  @flat.save!
+}
